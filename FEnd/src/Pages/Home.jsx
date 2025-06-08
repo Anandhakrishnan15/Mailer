f3f39f60943e509ceb73e79e6ce976e60cac7068
GirishEgo <Jai.girishego@gmail.com>
2025-06-08 18:56:13 +0530

    daynmic tabe added

--- FEnd/src/Pages/Home.jsx
+++ FEnd/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import DynamicTable from '../Components/DynamicTable'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <DynamicTable/>
     </div>
   )
 }
