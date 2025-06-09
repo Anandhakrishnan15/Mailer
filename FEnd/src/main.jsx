a6c52050c104e8f70d7741900e38f1f82a3df0ee
GirishEgo <Jai.girishego@gmail.com>
2025-06-09 22:43:12 +0530

    row provider context

--- FEnd/src/main.jsx
+++ FEnd/src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx';
+import { RowProvider } from './context/RowContext.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <RowProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </RowProvider>
     </BrowserRouter>
   </StrictMode>
 );
