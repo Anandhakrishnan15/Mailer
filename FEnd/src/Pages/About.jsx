2921264c09668b0b200bae9fe209f3ac7a9da8b4
GirishEgo <Jai.girishego@gmail.com>
2025-06-18 14:00:02 +0530

    remover glasscard

--- FEnd/src/Pages/About.jsx
+++ FEnd/src/Pages/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GlassCard from '../../../../cv/Portfolio/src/components/Props/GlassCardDocs/GlassCard'
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
-
+{/* <GlassCard/> */}
     </div>
   )
 }
